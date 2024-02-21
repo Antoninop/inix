c66394b91c6332089e1157cc037b8f152ccc0398
Antonino <antoninopiraino70pgmail.com>
2024-02-22 00:15:27 +0100

    amélioration style de la page de UsernameInputPage ajout systeme erreur pseudo

--- src/Components/UsernameInputPage/UsernameInputPage.jsx
+++ src/Components/UsernameInputPage/UsernameInputPage.jsx
@@ -1,9 +1,10 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './UsernameInputPage.module.scss';
 
 function PagePseudo({ setUsername }) {
     const [pseudo, setPseudo] = useState('');
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const savedPseudo = localStorage.getItem('pseudo');
@@ -15,23 +16,35 @@ function PagePseudo({ setUsername }) {
 
     const handlePseudoSubmit = (e) => {
         e.preventDefault();
+
+        if (!pseudo.trim()) {
+            setError('Veuillez entrer un pseudo valide.');
+            return;
+        }
+
+        setError(null);
         localStorage.setItem('pseudo', pseudo);
         setUsername(pseudo);
     };
 
     return (
-        <div className={styles.pagePseudoContainer}>
-            <h2>Saisir votre pseudo</h2>
-            <form onSubmit={handlePseudoSubmit}>
-                <input
-                    type="text"
-                    placeholder="Entrez votre pseudo"
-                    value={pseudo}
-                    onChange={(e) => setPseudo(e.target.value)}
-                    className={styles.inputField}
-                />
-                <button type="submit" className={styles.submitButton}>Valider</button>
-            </form>
+        <div className={styles.Components}>
+            <div className={styles.pagePseudoContainer}>
+                <h2>Saisir votre pseudo</h2>
+                <form onSubmit={handlePseudoSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Entrez votre pseudo"
+                        value={pseudo}
+                        onChange={(e) => setPseudo(e.target.value)}
+                        className={styles.inputField}
+                        aria-label="Pseudo"
+                        required
+                    />
+                    <button type="submit" className={styles.submitButton}>Valider</button>
+                </form>
+                {error && <p className={styles.errorMsg}>{error}</p>}
+            </div>
         </div>
     );
 }
